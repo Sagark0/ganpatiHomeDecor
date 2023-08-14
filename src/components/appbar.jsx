9968f5f509bf0cf026428978efa2c23dacd7a1c4
Light <sagarofficial2@gmail.com>
2023-08-14 13:39:31 +0530

    added responsiveness for mobile device

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -1,45 +1,58 @@
-import './appbar.css';
-import { useState, useEffect } from 'react';
+import "./appbar.css";
+import { useState, useEffect } from "react";
 
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function AppBar() {
-    const [scrolling, setScrolling] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
 
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolling(true);
-      } else {
-        setScrolling(false);
-      }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-  
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-    const headerClassName = scrolling ? 'sticky' : null;
-
+  }, []);
+  const handleMenuToggle = () => {
+    setMenuToggle((prev) => !prev);
+  };
+  const headerStickyActive = scrolling ? "sticky" : null;
+  const menuToggleActive = menuToggle ? "active" : null;
   return (
-    <header className={headerClassName}>
-
-        <a className='logo'>Ganpati Home Decor <span>.</span></a>
-        <IconButton
-            size="large"
-            edge="start"
-            aria-label="menu"
-            sx={{color: '#fff' }}
-          >
-            <MenuIcon />
-          </IconButton>
-        <ul className="nav">
-            <li><a>Home</a></li>
-            <li><a>Our Work</a></li>
-            <li><a>Contact Us</a></li>
-        </ul>
+    <header className={headerStickyActive}>
+      <a className="logo">
+        Ganpati Home Decor <span>.</span>
+      </a>
+      <IconButton
+        className={`menuToggle ${menuToggleActive} ${headerStickyActive}`}
+        size="large"
+        edge="start"
+        aria-label="menu"
+        sx={{ color: "#fff" }}
+        onClick={handleMenuToggle}
+      >
+        {menuToggleActive ? <CloseIcon /> : <MenuIcon />}
+      </IconButton>
+      <ul className={`nav ${menuToggleActive}`}>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Our Work</a>
+        </li>
+        <li>
+          <a>Contact Us</a>
+        </li>
+      </ul>
     </header>
   );
 }
